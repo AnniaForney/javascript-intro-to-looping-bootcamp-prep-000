673cbf9298d3f3de1618e239273367735539e875
Annia Forney <anniaforney@gmail.com>
2019-03-27 22:35:10 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -26,7 +26,7 @@ function doWhileLoop(array) {
 
 	do {
 		console.log('array.length = ' + array.length + ' and i = ' + i);
-    array = array.slice(1)
+    array = arrslice(1)
 		incrementVariable()
 	} while (array.length > 0 && i < 5);
 	return array;
